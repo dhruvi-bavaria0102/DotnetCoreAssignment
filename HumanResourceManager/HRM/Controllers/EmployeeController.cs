aa4efd4b447f7a00cb32adf10f4162c4d5aa33ed
Dhruvi Bavaria <dhruvi.bavaria@gatewaygroup.com>
2021-03-22 17:27:45 +0530

    first commit

---
+++ HumanResourceManager/HRM/Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using HRM.BAL.Manager;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRM.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        IEmployeeManager _employeeManager;
+
+        public EmployeeController(IEmployeeManager employeeManager)
+        {
+            _employeeManager = employeeManager;
+        }
+        public IActionResult Index()
+        {
+            return View(_employeeManager.GetAllEmployees());
+        }
+    }
+}
